5d0dd1affd15e2688ddea282726ee20dcce03bc9
Daniel Alejandro Palma Garcia <danielpalma2003@hotmail.com>
2024-04-04 16:01:30 -0500

    hotfix: Solved ENOENT: no such file or directory, stat '/opt/html/index.html'

--- app/controller/index/index.controller.ts
+++ app/controller/index/index.controller.ts
@@ -3,7 +3,7 @@ import path from "path";
 
 export async function controller(req: Request, res: Response) {
   try {
-    res.sendFile(path.resolve("../../../html/index.html"));
+    res.sendFile(path.resolve(__dirname + "../../../html/index.html"));
   } catch (Error) {
     console.error("Error while getting index.html" + Error)
   }
